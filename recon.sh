86ce9de3e3f1df4e6667c9513580fec2f25e7eb8
nullenc0de <pseekamp@gmail.com>
2020-02-03 14:04:39 -0500

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -22,6 +22,7 @@ mkdir ~/Research/Targets/$1
 mkdir ~/Research/Targets/$1/Shodan
 mkdir ~/Research/Targets/$1/GitHub
 mkdir ~/Research/Targets/$1/Screenshots
+mkdir ~/Research/Targets/$1/Wapiti
 mkdir ~/Research/Targets/$1/Endpoints
 mkdir ~/Research/Targets/$1/Archived
 mkdir ~/Research/Targets/$1/SubdomainTakeover
@@ -231,7 +232,7 @@ done
 
 ## LAUNCH WAPITI
 echo -e "\nRUNNING \e[31m[WAPITI]\e[0m"
-cat ~/Research/Targets/$1/Endpoints/unique-endpoints.txt | while read url; do wapiti --scope url --flush-session -u "$url"/ -f txt -o ~/Research/Targets/$1/wapiti-$(echo $url | cut -d\? -f1 | sed 's/\//_/g' | sed 's/\:/_/g').txt ;done
+cat ~/Research/Targets/$1/Endpoints/unique-endpoints.txt | while read url; do wapiti --scope url --flush-session -u "$url"/ -f txt -o ~/Research/Targets/$1/Wapiti/wapiti-$(echo $url | cut -d\? -f1 | sed 's/\//_/g' | sed 's/\:/_/g').txt ;done
 echo "RUNNING WAPITI \e[32mFINISH\e[0m"
 
 mv ~/Research/Targets/$1/$1.amasspassive.txt ~/Research/Targets/$1/$1.assetfinder.txt ~/Research/Targets/$1/$1.dnsbuffer.txt ~/Research/Targets/$1/$1.domain.txt ~/Research/Targets/$1/$1.livehosts-strip.txt ~/Research/Targets/$1/$1.massdns.txt ~/Research/Targets/$1/$1.probed.txt ~/Research/Targets/$1/$1.resolved.txt ~/Research/Targets/$1/$1.root.txt ~/Research/Targets/$1/$1.txt ~/Research/Targets/$1/ip.txt ~/Research/Targets/$1/$1.alldomains.txt ~/Research/Targets/$1/$1.subfinder.txt ~/Research/Targets/$1/$1.Crawler.txt ~/Research/Targets/$1/$1.all-final.txt ~/Research/Targets/$1/$1.gobuster.txt ~/Research/Targets/$1/Archived
