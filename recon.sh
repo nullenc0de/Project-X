a297af721be6c3f33cda0a378e07588b0cb4b5c6
nullenc0de <pseekamp@gmail.com>
2020-02-02 21:32:44 -0500

    wap wap

--- recon.sh
+++ recon.sh
@@ -212,6 +212,11 @@ for i in `cat ~/Research/Targets/$1/$1.livehosts-strip.txt`; do
         fi
 done
 
+## LAUNCH WAPITI
+echo -e "\nRUNNING \e[31m[WAPITI]\e[0m"
+cat ~/Research/Targets/hulu.com/Endpoints/unique-endpoints.txt | while read url; do wapiti --scope url --flush-session -u "$url"/ -f txt -o ~/Research/Targets/$1/wapiti_$url ;done
+echo "RUNNING WAPITI \e[32mFINISH\e[0m"
+
 ## REMOVING UNUSED FILES
 #rm ~/Research/Targets/$1/$1.amasspassive.txt ~/Research/Targets/$1/$1.assetfinder.txt ~/Research/Targets/$1/$1.dnsbuffer.txt ~/Research/Targets/$1/$1.domain.txt ~/Research/Targets/$1/$1.livehosts-strip.txt ~/Research/Targets/$1/$1.massdns.txt ~/Research/Targets/$1/$1.probed.txt ~/Research/Targets/$1/$1.resolved.txt ~/Research/Targets/$1/$1.root.txt ~/Research/Targets/$1/$1.txt ~/Research/Targets/$1/ip.txt ~/Research/Targets/$1/$1.alldomains.txt
 
